c40b74a4b7e4dd05894988186176b3df7b4ca860
Elyott Ryan <elyott_ryan@hotmail.com>
2018-05-04 22:42:04 +0000

    added create recipe component to recipes

--- app/javascript/bundles/Recipes/components/CreateRecipe.jsx
+++ app/javascript/bundles/Recipes/components/CreateRecipe.jsx
@@ -4,17 +4,7 @@ export default class CreateRecipe extends React.Component {
 
     render() {
     return (
-      <div>
-        <nav className="navbar">
-          <a href="/" className="navbar-brand">Spork</a>
-          <div className="my-recipes">
-            My Recipes
-          </div>
-          <div className="my-recipes">
-            Login
-          </div>
-        </nav>
-
+      <div className="new-recipe">
         <h1>CREATE A NEW RECIPE!</h1>
       </div>
     );
--- app/javascript/bundles/Recipes/components/Recipes.jsx
+++ app/javascript/bundles/Recipes/components/Recipes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './Navbar.jsx'
+import CreateRecipe from './CreateRecipe.jsx'
 
 export default class Recipes extends React.Component {
 
@@ -8,10 +9,11 @@ export default class Recipes extends React.Component {
       <div>
         <Navbar />
         <div className="container">
-          <div className="recipe-card card">
+        <CreateRecipe />
+          <div className="recipe-card card border-dark">
             <img className="card-img-top" src="/recipe/salmon.jpg" alt="Card image cap"/>
             <div className="card-body">
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p className="card-title h3 text-center">Salmon Teriyaki Dinner</p>
             </div>
           </div>
           <div className="recipe-card card">
