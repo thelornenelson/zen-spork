6ead2c7633d5755b934f8b76cd55da798fcc43a1
Craig Rice <craigricetong@gmail.com>
2018-05-06 14:59:15 +0000

    first working version of full screen enter and exit

--- app/javascript/bundles/Recipes/components/Recipes.jsx
+++ app/javascript/bundles/Recipes/components/Recipes.jsx
@@ -1,15 +1,65 @@
-import React from 'react';
-import Navbar from './Navbar.jsx'
-import CreateRecipe from './CreateRecipe.jsx'
+import React from "react"
+import Navbar from "./Navbar.jsx"
+import CreateRecipe from "./CreateRecipe.jsx"
+// Full screen module
+import Fullscreen from "react-full-screen"
+// Single recipe component, for cooking view
+import SingleRecipe from "./SingleRecipe.jsx"
 
 export default class Recipes extends React.Component {
+  constructor(props) {
+    super(props)
+    // Boolean state for hiding components on clicks
+    this.state = {
+      isFull: false,
+      cookingView: false,
+    }
+  }
 
-    render() {
+  // Called whenever we want to go to full screen
+  goFull = () => {
+    this.setState({ isFull: true })
+  }
+
+  mirrorGoFullFalse = () => {
+    if (this.state.isFull && this.state.cookingView) {
+      console.log("FULL SCREEN")
+    } 
+    if (!this.state.isFull && this.state.cookingView) {
+      this.setState({ cookingView: false }) && console.log("FALSE")
+      console.log("FALSE")
+    }  
+  }
+
+  // // method for pressing escape and logging how handsome I am
+  // escFunction(event) {
+  //   if (event.keyCode === 27) {
+  //     this.setState({ cookingView: false })
+  //     console.log("hi handsome")
+  //   }
+  // }
+
+  // mounting and unmounting escape method after DOM rendered
+  // componentDidMount() {
+  //   document.addEventListener("keydown", this.escFunction, false)
+  // }
+  // componentWillUnmount() {
+  //   document.removeEventListener("keydown", this.escFunction, false)
+  // }
+  componentDidUpdate() {
+    this.mirrorGoFullFalse()
+  }
+
+
+  render() {
     return (
       <div>
+        {/* Boolean to hide/reveal recipe when button clicked */}
+        { this.state.cookingView ? <SingleRecipe /> : null }
+       
         <Navbar />
         <div className="container">
-        <CreateRecipe />
+          <CreateRecipe />
           <div className="recipe-card card border-dark">
             <img className="card-img-top" src="/recipe/salmon.jpg" alt="Card image cap"/>
             <div className="card-body">
@@ -34,7 +84,12 @@ export default class Recipes extends React.Component {
                   Some quick example text to build on the card title and make up the bulk of the card's content.
                 </div>
                 <div className="modal-footer">
-                  <button type="button" className={"btn btn-secondary"} data-dismiss="modal">Spork</button>
+                  <button type="button" className={"btn btn-secondary"} data-dismiss="modal" onClick={this.goFull}>Spork</button>
+                  {/*  magically necessary to make the browser fullscreen */}
+                  <Fullscreen 
+                    enabled={this.state.isFull} 
+                    onChange={isFull => this.setState({ isFull, cookingView : true })} >
+                  </Fullscreen>
                 </div>
               </div>
             </div>
@@ -51,7 +106,7 @@ export default class Recipes extends React.Component {
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
-           <div className={"recipe-card card"}>
+          <div className={"recipe-card card"}>
             <img className="card-img-top" src="/recipe/salmon.jpg" alt="Card image cap"/>
             <div className="card-body">
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -71,6 +126,6 @@ export default class Recipes extends React.Component {
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
---
+++ app/javascript/bundles/Recipes/components/SingleRecipe.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        testing single recipe
+
+      </div>
+    )
+  }
+}
