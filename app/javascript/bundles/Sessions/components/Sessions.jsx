5800f38dbf3bc9563e6493b3c759bcabef50d291
Phil Werner <philwerner@shaw.ca>
2018-05-08 19:08:19 +0000

    Cleaned up some code

--- app/javascript/bundles/Sessions/components/Sessions.jsx
+++ app/javascript/bundles/Sessions/components/Sessions.jsx
@@ -3,35 +3,31 @@ import Navbar from "../../Recipes/components/Navbar.jsx"
 
 export default class Sessions extends React.Component {
 
-    render() {
+  render() {
 
     return (
-        <div>
+      <div>
 
-          <Navbar current_user={this.props.current_user} />
+        <Navbar current_user={this.props.current_user} />
 
+        <div className="login-box">
 
-            <div className="login-box">
+          <h1>Please log in!</h1>
 
-              <h1>Please log in!</h1>
+          <form method="post" action="/login">
+            <div className="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
 
-              <form method="post" action="/login">
-                <div className="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-
-                </div>
-                <div className="form-group">
-                  <label for="exampleInputPassword1">Password</label>
-                  <input type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-                </div>
-                  <button type="submit" className="btn btn-primary">Submit</button>
-              </form>
             </div>
+            <div className="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
         </div>
+      </div>
     );
   }
 }
-
-
-//add box maybe
