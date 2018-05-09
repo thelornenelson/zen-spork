a4462ee1f069675862aba7c345c0aeae05abb5dc
Craig Rice <craigricetong@gmail.com>
2018-05-09 18:56:30 +0000

    random photo from factory bot is coming up on page load with some being blank strings

--- app/javascript/bundles/Recipes/components/RecipeIndex.jsx
+++ app/javascript/bundles/Recipes/components/RecipeIndex.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import LearnMore from "./LearnMore.jsx";
 
+// The recipe cards that show up on the homepage view
 export default class RecipeIndex extends React.Component {
   render() {
     const title = this.props.recipe.title;
-    console.log("RECIPE =", this.props.recipe);
+    const photo = this.props.recipe.photo_url;
+    console.log("RECIPE", this.props.recipe);
+    console.log("PHOTO CONST", photo);
     return (
       <article>
         <div className="recipe-card card border-dark">
-          <img className="card-img-top" src="/recipe/salmon.jpg" alt="salmon" />
+          <img className="card-img-top" src={photo} alt="food porn of cooked dish" />
           <div className="card-body">
             <p className={"card-title h3 text-center"}>{ title }</p>
             <LearnMore recipe={this.props.recipe} toggleDetailView={this.props.toggleDetailView}/>     
