ec47a22812ae750743b38b79181d6a8feb46a5ee
Andrew Barclay <andrewkbarclay@gmail.com>
2018-05-15 22:13:31 +0000

    edit and cook buttons now work for correct recipe

--- app/javascript/bundles/Recipes/components/DetailedPopup.jsx
+++ app/javascript/bundles/Recipes/components/DetailedPopup.jsx
@@ -196,10 +196,10 @@ export default class DetailedPopup extends React.Component {
               </div>
               <div className="container modal-footer DPU-buttons">
                 <div className="row">
-                  <FullScreenButton recipe={this.props.recipe} multi={this.state.servingMultiplier} />
+                  <FullScreenButton recipe={this.props.getRecipeById(recipe.id)} multi={this.state.servingMultiplier} />
                   {/* Hide spork button if not logged in, or it's your recipe you're viewing */}
                   {this.props.current_user_id !== recipe.user_id && this.props.current_user_id && <button type="button" className={"btn btn-primary"} onClick={(e) => { this.props.sporkRecipe(this.props.recipe, e); this.props.onClose(); }}><i className="fas fa-clone"></i> Spork</button>}
-                  {this.props.current_user_id === recipe.user_id && <button type="button" name="editRecipe" className={"btn btn-primary"} onClick={(e) => {this.props.toggleViews(e, this.props.recipe);}}><i className="fas fa-edit"></i> Edit</button>}
+                  {this.props.current_user_id === recipe.user_id && <button type="button" name="editRecipe" className={"btn btn-primary"} onClick={(e) => {this.props.toggleViews(e, this.props.getRecipeById(recipe.id));}}><i className="fas fa-edit"></i> Edit</button>}
                 </div>
                 <div className="row">
                   <form>
