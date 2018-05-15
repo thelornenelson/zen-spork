3ce9819181518f32f8a4a1f249ad5586c7d3364e
Craig Rice <craigricetong@gmail.com>
2018-05-15 18:31:44 +0000

    initial working in NavBar, but hacky

--- app/javascript/bundles/Recipes/components/Navbar.jsx
+++ app/javascript/bundles/Recipes/components/Navbar.jsx
@@ -2,16 +2,21 @@ import React from "react";
 
 export default class Navbar extends React.Component {
   render() {
+    const firstInitial = this.props.current_user_name ? this.props.current_user_name.charAt(0) : "";
+    const lastInitial = this.props.current_user_last_name ? this.props.current_user_last_name.charAt(0) : "";
+    const userInitials = firstInitial + lastInitial;
     // Makes the button display either My Recipes or All recipes depending on the state
     const myRecipesToggleName = ((this.props.myRecipesView)
       ? (<button type="button" className="btn btn-primary" name="myRecipesView" onClick={this.props.toggleViews}>All Recipes</button>)
       : (<button type="button" className="btn btn-primary" name="myRecipesView" onClick={this.props.toggleViews}>My Recipes</button>));
 
     const loginToggle = ((this.props.current_user)
-      ?(<div>{myRecipesToggleName}
+      ?(<div className="login-buttons">
+        <div className="nav-user">{userInitials}</div>
+        {myRecipesToggleName}
         <button type="button" className="btn btn-primary" name="createRecipe" onClick={this.props.toggleViews}>New Recipe</button>
         <a href="/logout" className="btn btn-primary">Sign Out</a>
-        <div className="nav-user">{this.props.current_user_name}</div></div>)
+      </div>)
       : (<div className="login-buttons"><a href="/login" className="btn btn-primary">Log In</a>
         <a href="/signup" className="btn btn-primary">Sign Up</a></div>));
 
