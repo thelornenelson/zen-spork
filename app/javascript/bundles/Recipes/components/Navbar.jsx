452ddf0bbc2882aee382f32b32a199d31ce2b87f
Craig Rice <craigricetong@gmail.com>
2018-05-15 16:57:16 +0000

    login and sign up buttons updated

--- app/javascript/bundles/Recipes/components/Navbar.jsx
+++ app/javascript/bundles/Recipes/components/Navbar.jsx
@@ -4,16 +4,16 @@ export default class Navbar extends React.Component {
   render() {
     // Makes the button display either My Recipes or All recipes depending on the state
     const myRecipesToggleName = ((this.props.myRecipesView)
-      ? (<button type="button" className="btn nav-button" name="myRecipesView" onClick={this.props.toggleViews}>All Recipes</button>)
-      : (<button type="button" className="btn nav-button" name="myRecipesView" onClick={this.props.toggleViews}>My Recipes</button>));
+      ? (<button type="button" className="btn btn-primary" name="myRecipesView" onClick={this.props.toggleViews}>All Recipes</button>)
+      : (<button type="button" className="btn btn-primary" name="myRecipesView" onClick={this.props.toggleViews}>My Recipes</button>));
 
     const loginToggle = ((this.props.current_user)
       ?(<div>{myRecipesToggleName}
-        <button type="button" className="btn nav-button" name="createRecipe" onClick={this.props.toggleViews}>New Recipe</button>
-        <a href="/logout" className="btn nav-button">Sign Out</a>
+        <button type="button" className="btn btn-primary" name="createRecipe" onClick={this.props.toggleViews}>New Recipe</button>
+        <a href="/logout" className="btn btn-primary">Sign Out</a>
         <div className="nav-user">{this.props.current_user_name}</div></div>)
-      : (<div><a href="/signup" className="btn nav-button">Sign Up</a>
-        <a href="/login" className="btn nav-button">Sign In</a></div>));
+      : (<div className="login-buttons"><a href="/login" className="btn btn-primary">Log In</a>
+        <a href="/signup" className="btn btn-primary">Sign Up</a></div>));
 
     return (
       <nav className="navbar">
