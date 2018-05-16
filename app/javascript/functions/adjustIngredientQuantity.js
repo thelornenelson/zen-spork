9c4420921b4415765ba799186882ee6bd36b2ef3
Phil Werner <philwerner@shaw.ca>
2018-05-16 16:45:31 +0000

    Can now scale quantities with dashes

--- app/javascript/functions/adjustIngredientQuantity.js
+++ app/javascript/functions/adjustIngredientQuantity.js
@@ -4,28 +4,49 @@ export function adjustIngredientQuantity(quantity, multiplier) {
 
   let math = require('mathjs');
 
+  let output = "";
+
   if (quantity.length === 0) {
     return "";
   }
 
-  //Here we test that there are no letters in the quantity, and also that there isn't more than 1 backslash.
+  //Here we test that there are no letters in the quantity, and also that there isn't more than 1 backslash or dash.
   //If there are it just returns the original quantity string.
-  let numBackSlahes = 0
+  let numBackSlahes = 0;
+  let numDashes = 0;
   for (let i = 0; i < quantity.length; i++) {
-    if (quantity[i] !== "0" && quantity[i] !== "1" && quantity[i] !== "2" && quantity[i] !== "3" && quantity[i] !== "4" && quantity[i] !== "5" && quantity[i] !== "6" && quantity[i] !== "7" && quantity[i] !== "8" && quantity[i] !== "9" && quantity[i] !== "/" && quantity[i] !== " ") {
+    if (quantity[i] !== "0" && quantity[i] !== "1" && quantity[i] !== "2" && quantity[i] !== "3" && quantity[i] !== "4" && quantity[i] !== "5" && quantity[i] !== "6" && quantity[i] !== "7" && quantity[i] !== "8" && quantity[i] !== "9" && quantity[i] !== "/" && quantity[i] !== " " && quantity[i] !== "-" && quantity[i] !== ".") {
       return quantity;
     }
     if (quantity[i] === "/"){
       numBackSlahes++;
       if (numBackSlahes > 1) return quantity;
     }
+
+    if (quantity[i] === "-") {
+      numDashes++;
+      if (numDashes > 1) return quantity;
+    }
   }
 
-  let output = "";
+  //Here, if a dash is used (ie someone enters 4-5 onions) it multiplies both numbers
+  if (numDashes === 1) {
+    for (let i = 0; i < quantity.length; i++) {
+      if (quantity[i] === "-") {
+        let firstNum = quantity.slice(0, i);
+        let secondNum = quantity.slice(i+1, quantity.length);
+        firstNum = firstNum * multiplier;
+        secondNum = secondNum * multiplier;
+        output = output + firstNum + "-" + secondNum;
 
-    let fract = math.fraction(quantity);
-    fract = math.multiply(fract, multiplier);
-    output = convertFractionToString(fract);
+        return output;
+      }
+    }
+  }
+
+  let fract = math.fraction(quantity);
+  fract = math.multiply(fract, multiplier);
+  output = convertFractionToString(fract);
 
   return output;
 }
